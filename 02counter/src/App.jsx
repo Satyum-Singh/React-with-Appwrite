52fec069ab34e266d5de7de84dff98e0841da03a
Satyum <satyumspathania@gmail.com>
2024-02-24 15:44:03 +0530

    updating counter, learned about states and batching issues and remedies.

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [counter, setCounter] = useState(0);
+  const addValue = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+  const removeValue = () => setCounter(counter - 1);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Course with Satyum</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add value</button > {" "}
+      <button onClick={removeValue} > Remove value</button >
+      <p>Footer : {counter}</p>
     </>
   )
 }
