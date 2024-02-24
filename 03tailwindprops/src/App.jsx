37c4983cce1b0f9b0fc5907f9900f3f9b5e07e30
Satyum <satyumspathania@gmail.com>
2024-02-24 17:55:27 +0530

    Learned about components and props.

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
 import Card from './components/Card'
 
 function App() {
   return (
-    <>
+    <div>
       <h1 className='text-3xl bg-green-500 p-3 rounded-md' >Vite with Tailwind</h1>
-      <Card />
-    </>
+      <Card username="Satyum" />
+    </div>
   )
 }
 
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,8 +1,11 @@
-export const Card = () => {
+function Card({ username }) {
     return (
         <div>
-            <img src="https://unsplash.com/photos/a-building-with-a-window-and-a-door-rjslAIgMomM" />
-            <h1 className='text-2xl bg-green-500 p-3 rounded'> Card for photo</h1>
+            <img src="https://plus.unsplash.com/premium_photo-1680553492268-516537c44d91?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <h1 className='text-2xl bg-green-500 p-3 rounded'> Card for photo of {username}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum distinctio nobis molestias assumenda id incidunt temporibus, rem perspiciatis vel quod dolor. Perspiciatis, quis. Animi aliquam, nam consequuntur voluptatum odit dignissimos.</p>
         </div>
     )
 }
+
+export default Card
