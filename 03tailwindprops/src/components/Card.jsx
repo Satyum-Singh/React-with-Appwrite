68ae510592f51efb274fc365f5210ce61baed462
Satyum <satyumspathania@gmail.com>
2024-02-24 17:08:02 +0530

    learning about components

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,8 @@
+export const Card = () => {
+    return (
+        <div>
+            <img src="https://unsplash.com/photos/a-building-with-a-window-and-a-door-rjslAIgMomM" />
+            <h1 className='text-2xl bg-green-500 p-3 rounded'> Card for photo</h1>
+        </div>
+    )
+}
