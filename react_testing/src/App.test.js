f4dd472086a3f8d219503d2e22037cb10dcb4d1b
Satyum <satyumspathania@gmail.com>
2024-07-09 05:17:38 +0530

    studied about describe.

--- react_testing/src/App.test.js
+++ react_testing/src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Test React app case 1", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/First React Test case/i);
+  expect(text).toBeInTheDocument();
+});
+
+test("Testing input box case 2", () => {
+  render(<App />);
+  let checkInput = screen.getByRole("textbox");
+  expect(checkInput).toBeInTheDocument();
+  const placeholder = screen.getByPlaceholderText("Enter username");
+  expect(placeholder).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name","username");
+});
+
+// Grouping is done based upon diff types of test cases I want to run, UI, API, database etc.
+describe("UI Test groups",()=>{
+  test("ui case 1",()=>{
+    render(<App />)
+    let checkInput = screen.getByRole("textbox");
+    expect(checkInput).toBeInTheDocument();
+  })
+})
+
+describe("API Test groups", () => {
+  test("api case 1", () => {
+    render(<App />);
+    let checkInput = screen.getByRole("textbox");
+    expect(checkInput).toBeInTheDocument();
+  });
 });
