6ccde94705d37b6b1cd1f9d047ff8c238ee8bf7c
Satyum <satyumspathania@gmail.com>
2024-07-13 09:19:29 +0530

    react_testing

--- react_testing/src/App.js
+++ react_testing/src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data,setData] = useState("")
+
   return (
     <div className="App">
       {/* Test case 1 */}
-      <p>First React Test casengjonij</p>
+      <p>First React Test case</p>
 
       {/* Test case 2 */}
       <input type="text" placeholder="Enter username" name="username" />
+
+      {/* Test case 3 */}
+      <input type="text" onChange={(e)=>setData(e.target.value)} value={data} />
     </div>
   );
 }
--- react_testing/src/App.test.js
+++ react_testing/src/App.test.js
@@ -1,34 +1,42 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// TestCase 1: Testing the text in the component
 test("Test React app case 1", () => {
   render(<App />);
   const text = screen.getByText(/First React Test case/i);
   expect(text).toBeInTheDocument();
 });
 
-test("Testing input box case 2", () => {
+// test("Testing input box case 2", () => {
+//   render(<App />);
+//   let checkInput = screen.getByRole("textbox");
+//   expect(checkInput).toBeInTheDocument();
+//   const placeholder = screen.getByPlaceholderText("Enter username");
+//   expect(placeholder).toBeInTheDocument();
+//   expect(checkInput).toHaveAttribute("name","username");
+// });
+
+// TestCase 2: Grouping is done based upon diff types of test cases I want to run, UI, API, database etc.
+// describe("UI Test groups",()=>{
+//   test("ui case 1",()=>{
+//     render(<App />)
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+//   })
+// })
+
+// describe("API Test groups", () => {
+//   test("api case 1", () => {
+//     render(<App />);
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+//   });
+// });
+
+// TestCase 3 : event handling testing
+test("OnChange event test", () => {
   render(<App />);
-  let checkInput = screen.getByRole("textbox");
-  expect(checkInput).toBeInTheDocument();
-  const placeholder = screen.getByPlaceholderText("Enter username");
-  expect(placeholder).toBeInTheDocument();
-  expect(checkInput).toHaveAttribute("name","username");
-});
-
-// Grouping is done based upon diff types of test cases I want to run, UI, API, database etc.
-describe("UI Test groups",()=>{
-  test("ui case 1",()=>{
-    render(<App />)
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-  })
-})
-
-describe("API Test groups", () => {
-  test("api case 1", () => {
-    render(<App />);
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-  });
+  let field = screen.getByRole("textbox");
+  expect(field).toBeInTheDocument();
 });
